6df57c6215a75715d261533a14cb0f3723c2eed0
Paul Taylor <pltorigami@gmail.com>
2021-05-13 12:26:34 -0400

    added docker volumes for data and made db/es services internal

--- runIt.py
+++ runIt.py
@@ -1,5 +1,31 @@
+import os
+import time
+
+import requests
+from requests.exceptions import ConnectionError
+
 from webApp import app, db
 
 if __name__ == "__main__":
+    esUrl = os.environ.get("FLASK_FORUM_ES_URL")
+
+    # wait for elasticsearch service to be ready before starting server
+
+    print(f"waiting for elasticsearch service to be ready by querying {esUrl}...")
+
+    while True:
+        try:
+            time.sleep(2)
+            elasticRequest = requests.get(f"http://{esUrl}")
+            if elasticRequest.status_code >= 200 and elasticRequest.status_code < 500:
+                break
+        except ConnectionError:
+            pass
+
+    print("elasticsearch ready, creating database schema...")
+
     db.create_all()
+
+    print("database schema created, running server...")
+
     app.run(host="0.0.0.0", debug=True)
--- webApp/__init__.py
+++ webApp/__init__.py
@@ -10,10 +10,8 @@ app.config["SECRET_KEY"] = os.environ.get("FLASK_FORUM_SECRET_KEY")
 postgresPwd = os.environ.get("POSTGRES_PASSWORD")
 postgresUser = os.environ.get("POSTGRES_USER")
 postgresDb = os.environ.get("POSTGRES_DB")
-# postgresqlStr = f"postgresql://{postgresUser}:{postgresPwd}@localhost:5432/postgres"
 postgresqlStr = f"postgresql://{postgresUser}:{postgresPwd}@db:5432/{postgresDb}"
 app.config["SQLALCHEMY_DATABASE_URI"] = postgresqlStr
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///other.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["ELASTICSEARCH_URL"] = os.environ.get("FLASK_FORUM_ES_URL")
 
